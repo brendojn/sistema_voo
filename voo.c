732c010e0bee7b9f8aca268998ca523660e4b9d4
brendojn <brendojnio@gmail.com>
2017-05-31 19:24:52 -0300

    CPF aleatório

--- voo.c
+++ voo.c
@@ -4,6 +4,14 @@
 #include <conio.h>
 #include <time.h>
 
+int gerarCpf(int *cpf) {
+	int i;
+	srand(time(NULL));
+	for (i = 0; i <= 350; i++) {
+	    printf("%i\n", 100000000 + rand() % 999999999);
+	}
+}
+
 int numerovoo(int *numero_voo){
 int i, mil, q, indice = 0;
 
@@ -21,11 +29,24 @@ int i, mil, q, indice = 0;
 
 
 int main(){
-    int numero_voo[50], i;
+    int cpf[350], numero_voo[50], i,assentos[500],x=0,pos=0;
     numerovoo(numero_voo);
-    for(i = 0; i < 50; i++){
+   /* for(i = 0; i < 50; i++){
         printf("\n %d", numero_voo[i]);
+    }*/
+    gerarCpf(cpf);
+
+    for(x;x<500;x++){
+        assentos[x]=0;
     }
+
+    for(x=0;x<350;x++){
+        pos = cpf[x]%500;
+        assentos[pos]=cpf[x];
+
+        printf("\nAssento na posicao %d recebeu %d",pos,cpf[x]);
+    }
+
 
 
 }
